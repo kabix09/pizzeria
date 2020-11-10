6a31aa29a24bb429aa7d11a62df940e9920c6351
kabix09 <kabix.009@gmail.com>
2020-11-10 18:32:15 +0100

    simple home page

---
+++ src/components/home/home.jsx
@@ -0,0 +1,9 @@
+const { Component } = require("react");
+
+class Home extends Component
+{
+    render(){
+        return(<h1 style={{textAlign: 'center'}}>Hello :)</h1>);
+    }
+}
+export default Home;
