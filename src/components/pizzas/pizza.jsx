20c735d57e6296f2831f3c070cfd261be0d8ebc6
kabix09 <kabix.009@gmail.com>
2020-11-18 23:52:51 +0100

    display ingredients name in pizza

--- src/components/pizzas/pizza.jsx
+++ src/components/pizzas/pizza.jsx
@@ -9,12 +9,43 @@ class Pizza extends Component
         super(props);
 
         this.state = {
-            pizza: this.props.pizza
+            pizza: this.props.pizza,
+            ingredients: []
         };    
+    }
+    componentDidMount()
+    {
+        this.fetchIngredients();
+    }
+
+    fetchIngredients()
+    {
+        const url = "http://localhost:3333/api/ingredient/";
+
+        fetch(url, {
+                    method: 'GET'
+            })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }else {
+                    throw new Error(`Http error: ${response.status}`);
+                }
+            })
+            .then(data => {
+                this.setState({ingredients: data});
+            })
+            .catch(error => {
+                console.error(error)
+            });        
     }
 
     render()
     {
+        const pizzaIngredients = this.state.pizza.ingredients.map((pizzaIngredient) => {
+            return this.state.ingredients.find(ingredient => ingredient.id === pizzaIngredient);
+        });
+        
         return(
             <Card style={{height: '300px'}}>
                 <Card.Content>
@@ -22,8 +53,8 @@ class Pizza extends Component
                     <Card.Description>
                         Ingredients:
                         <ul style={{maxHeight: '150px', overflow: 'auto'}}>
-                            {this.state.pizza.ingredients.map(
-                                ingredient => <li key={Math.random()}>{ingredient}</li>
+                            {pizzaIngredients.map(
+                                ingredient => ( ingredient !== undefined && <li key={Math.random()}>{ingredient.name}</li>)
                             )}
                         </ul>
                     </Card.Description>
