0a23f8f90ca470c6a06bf19225505e8542c62128
kabix09 <kabix.009@gmail.com>
2020-11-30 13:03:27 +0100

    add simple epic error handling

--- src/store/episc/init.epic.js
+++ src/store/episc/init.epic.js
@@ -1,16 +1,24 @@
 import { ofType } from 'redux-observable';
 import { map, switchMap, mergeMap } from 'rxjs/operators';
 import { fromFetch } from 'rxjs/fetch';
+import { catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import * as pizzaActions from '../data/pizza/pizza.constants';
 import * as ingredientActions from '../data/ingredient/ingredient.constants';
+import {FETCH_DATA_ERROR} from '../data/error/error.constants';
 
 export const epic = action$ => action$.pipe(
     ofType(pizzaActions.FETCH_PIZZAS, ingredientActions.FETCH_INGREDIENTS),
     mergeMap((action) => fromFetch(action.source)
         .pipe(
             switchMap(response => response.json()),
-            map(data => action.onSuccess(data))
+            map(data => action.onSuccess(data)),
+            catchError(error => of({
+                type: FETCH_DATA_ERROR,
+                payload: error,
+                error: true
+              }))
         )
     )
 );
--- src/store/init.reducer.js
+++ src/store/init.reducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import labelReducer from './data/label/label.reducers';
 import pizzaReducer from './data/pizza/pizza.reducers';
 import ingredientReducer from './data/ingredient/ingredient.reducers';
+import errorReducer from './data/error/error.reducers.js';
 
 export default combineReducers({
   label: labelReducer,
   pizzas: pizzaReducer,
-  ingredients: ingredientReducer
+  ingredients: ingredientReducer,
+  errors: errorReducer
 });
