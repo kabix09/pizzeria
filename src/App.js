e5deba009e43785cc6e0a45ebd8b2e824a5e4f60
kabix09 <kabix.009@gmail.com>
2020-12-18 11:31:05 +0100

    handling invalid routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import NavBar from './components/navBar';
 import Home from './components/home/home';
@@ -30,6 +30,8 @@ class App extends Component
             <Route path="/" exact component={Home}/>
             <Route path="/menu/:name?" render={(props) => (<Menu {...props} key={Math.random()}/>)}/>
             <Route path="/koszyk" component={BasketContainer}/>
+            
+            <Redirect to="/"/>
           </Switch>
         </BrowserRouter>
       </React.Fragment>
