6011a45e6dd301bb0341c3dd56790cf7744b54c6
kabix09 <kabix.009@gmail.com>
2021-01-31 00:58:03 +0100

    fix saga worker

--- src/store/sagas/init.saga.js
+++ src/store/sagas/init.saga.js
@@ -2,12 +2,18 @@ import { takeEvery, put } from 'redux-saga/effects';
 
 import * as initStore from '../init.constants';
 
+import * as error from '../data/error/error.constants';
+
 import { fetchPizzas } from '../data/pizza/pizza.actions';
 import { fetchIngredients } from '../data/ingredient/ingredient.actions';
 import { setLabel } from '../data/label/label.actions';
 import { initBasket } from '../data/basket/basket.actions';
 
+
 export function* worker(action) {
+
+    if(action.type == initStore.INITIALIZE)
+    {
         // set beginer label
         yield put(setLabel(window.location.pathname.split("/")[1]));
         
@@ -17,6 +23,7 @@ export function* worker(action) {
 
         // init basket
         yield put(initBasket);
+    }
 }
 
 export function* watcher(){
