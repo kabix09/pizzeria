c2d28f54f97b1e94392a5cbaf1373f3786fe5790
kabix09 <kabix.009@gmail.com>
2021-02-02 20:30:34 +0100

    change main page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,9 +1,22 @@
+import {Container, Segment, Header, Icon} from 'semantic-ui-react';
 const { Component } = require("react");
 
 class Home extends Component
 {
     render(){
-        return(<h1 style={{textAlign: 'center'}}>Hello :)</h1>);
+        return(
+            <Container style={{minHeight: '42rem',  padding: '3rem'}}>
+                <Segment placeholder style={{height: '42rem', background: `url(${process.env.PUBLIC_URL + '/pizzaWallpaper.jpg'})` }}>
+                    <Header icon as='h1'>
+                        <Icon name='food' size='huge'/>
+                        <Header >
+                            The best Pizza in Your town!!
+                        </Header>
+                    </Header>
+                </Segment>
+            </Container>
+            
+        );
     }
 }
 export default Home;
