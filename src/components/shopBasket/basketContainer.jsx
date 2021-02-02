fdddd653b6c434be83b4358f01cea5d9f89966f9
kabix09 <kabix.009@gmail.com>
2021-02-02 19:03:12 +0100

    add empty basket banner

--- src/components/shopBasket/basketContainer.jsx
+++ src/components/shopBasket/basketContainer.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Card, Container, Grid, Header} from 'semantic-ui-react';
+import {Card, Container, Grid, Header, Segment} from 'semantic-ui-react';
 import BasketElement from './basketElement';
 
 import OrderForm from '../form/orderForm';
@@ -21,10 +21,17 @@ class BasketContainer extends Component {
                     <Grid.Column width={7} style={{marginTop: '4rem'}}>
                         <Card.Group>
                         {
-                            (this.props.basket !== undefined && this.props.basket.length > 0) ?
-                                this.props.basket.map(
-                                    element => <BasketElement product={element} key={Math.random()}/>                                                        
-                                ):<div>no elements</div>
+                            (this.props.basket !== undefined && this.props.basket.length > 0) ? 
+                                    this.props.basket.map(
+                                        element => <BasketElement product={element} key={Math.random()}/>                                                        
+                                    )
+                                :
+                                    <Segment raised style={{ width: '25rem', height: '18rem', marginTop: '5rem', display: 'flex', justifyContent: 'space-around', alignItems: 'center',
+                                                            WebkitBoxShadow: '10px 10px 5px 0px rgba(0,0,0,0.3)', MozBoxShadow: '10px 10px 5px 0px rgba(0,0,0,0.3)', boxShadow: '10px 10px 5px 0px rgba(0,0,0,0.3)' }}>
+                                        <Header as='h2'>
+                                            Your basket is empty
+                                        </Header>
+                                    </Segment>
                         }
                         </Card.Group>
                     </Grid.Column>
